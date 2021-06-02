59d7d6802349453063226de6c7aa4692b341bd3e
magmom95 <64140544+magmom95@users.noreply.github.com>
2021-06-03 01:28:48 +0900

    Update 1546.py

--- baekJ/array/1546.py
+++ baekJ/array/1546.py
@@ -1,8 +1,8 @@
 N = int(input())
 M = list(map(int, input().split()))
-M_max = max(M)
-
+m = max(M)
+ 
 for i in range(N):
-    M[i] = M[i]/M_max*100
-avg = sum(M) / N
-print("%.2f" % avg)
+    M[i] = M[i]/m*100
+avg = sum(M)/ N
+print(f'{avg}')
