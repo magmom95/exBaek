9938ad31a2106089b8b0a4e6d7b15264ed2a1f0d
magmom95 <64140544+magmom95@users.noreply.github.com>
2021-06-04 14:40:32 +0900

    Add files via upload

---
+++ baekJ/Function1/function.py
@@ -0,0 +1,15 @@
+def d(n):
+    n = n+sum(map(int, str(n)))
+
+    return n
+
+
+a = [0]*10001
+
+for i in range(1, 10001):
+    a[i] = d(i)
+
+
+for i in range(1, 10001):
+    if i not in a:
+        print(i)
